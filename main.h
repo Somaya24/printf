fcb9a3086be6481f9833b9e7c9e1ae9e3d0bc1d8
bienfati <bienfati061@gmail.com>
2023-07-27 10:01:13 -0700

    tr

--- main.h
+++ main.h
@@ -1,42 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-int printIntegers(const char *format, ...);
-static int convertIntToString(int num, char *buffer);
-/**
-  * convertIntToString - converts an integer to a string and writes to buffer
-  * @num: the integer to convert
-  * @buffer: the buffer to store the string representation
-  * Return: the length of the string
-  */
-static int convertIntToString(int num, char *buffer)
-{
-int length = 0;
-if (num == 0)
-{
-buffer[length++] = '0';
-}
-else if (num < 0)
-{
-buffer[length++] = '-';
-num = -num;
-}
-int i;
 
-while (num != 0)
+int _printf(const char *format, ...);
+int (*check_specifier(const char *))(va_list);
+
+/**
+ * struct func - struct for specifier to printer
+ * @t: character to compare
+ * @f: function to handle printing
+ */
+typedef struct func
 {
-int digit = num % 10;
-buffer[length++] = digit + '0';
-num /= 10;
-}
-for (i = 0; i < length / 2; i++)
-{
-char temp = buffer[i];
-buffer[i] = buffer[length - i - 1];
-buffer[length - i - 1] = temp;
-}
-return (length);
-}
-#endif /* MAIN_H */
+char *t;
+int (*f)(va_list);
+} func_t;
+
+int print_char(va_list);
+int print_str(va_list);
+int print_cent(va_list);
+
+#endif
