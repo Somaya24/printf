0dc6ad593a12a8236e6478366c85db35a3cc09a2
Soumia <soumiae4@gmail.com>
2023-07-27 12:55:54 -0700

    update

--- main.h
+++ main.h
@@ -8,6 +8,7 @@
 int _printf(const char *format, ...);
 void print_Int(const char *format, ...);
 int print_binary(va_list list);
+int print_bnr(va_list arguments, char *buf, unsigned int ibuf);
 
 
 
