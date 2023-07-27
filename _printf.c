828b4552ae40b351fc56869cc52685df21bb8545
bienfati <bienfati061@gmail.com>
2023-07-27 12:23:52 -0700

    tr

--- _printf.c
+++ _printf.c
@@ -1,55 +1,30 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
 #include "main.h"
+
 /**
- * _printf - prints a string in a formatted way
- * @format: string to print (char *)
- * @...: variadic parameters (unknown)
+ * _printf - Produces output according to a format
+ * @format: Is a character string. The format string
+ * is composed of zero or more directives
  *
- * Return: number of characters printed
- */
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings)
+ **/
 int _printf(const char *format, ...)
 {
-int i = 0;
-int count = 0;
-int value = 0;
-va_list args;
-va_start(args, format);
-int (*f)(va_list);
-if (format == NULL)
-return (-1);
-while (format[i])
-{
-if (format[i] != '%')
-{
-value = write(1, &format[i], 1);
-count = count + value;
-i++;
-continue;
-}
-if (format[i] == '%')
-{
-f = check_specifier(&format[i + 1]);
-if (f != NULL)
-{
-value = f(args);
-count = count + value;
-i = i + 2;
-continue;
-}
-if (format[i + 1] == '\0')
-{
-break;
-}
-if (format[i + 1] != '\0')
-{
-value = write(1, &format[i + 1], 1);
-count = count + value;
-i = i + 2;
-continue;
-}
-}
-}
-return (count);
+	int size;
+	va_list args;
+
+	if (format == NULL)
+		return (-1);
+
+	size = _strlen(format);
+	if (size <= 0)
+		return (0);
+
+	va_start(args, format);
+	size = handler(format, args);
+
+	_putchar(-1);
+	va_end(args);
+
+	return (size);
 }
--- main.h
+++ main.h
@@ -1,27 +1,50 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-int _printf(const char *format, ...);
-int (*check_specifier(const char *))(va_list);
+/* utils.c */
+int _strlen(const char *);
+int print(char *);
+char *itoa(long int, int);
+
+/* printf.c */
+int _printf(const char *, ...);
+
+/* handler.c */
+int handler(const char *, va_list);
+int percent_handler(const char *, va_list, int *);
+
+/* printers */
+int print_string(va_list);
+int print_char(va_list);
+int print_integer(va_list);
+int print_binary(va_list);
+int print_rot(va_list);
+int print_unsigned(va_list);
+int print_octal(va_list);
+int print_hexadecimal_low(va_list);
+int print_hexadecimal_upp(va_list);
+int print_pointer(va_list);
+int print_rev_string(va_list);
+
+/* _putchar.c */
+int _putchar(char);
+int buffer(char);
 
 /**
- * struct func - struct for specifier to printer
- * @t: character to compare
- * @f: function to handle printing
- */
-typedef struct func
+ * struct _format - Typedef struct
+ *
+ * @type: Format
+ * @f: The function associated
+ **/
+typedef struct _format
 {
-char *t;
-int (*f)(va_list);
-} func_t;
+	char type;
+	int (*f)(va_list);
+} format;
 
-int print_char(va_list);
-int print_str(va_list);
-int print_cent(va_list);
 
 #endif
